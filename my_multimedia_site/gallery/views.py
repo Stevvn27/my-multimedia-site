d874d0f906addc82ac5fe1497e70a5fac99cf506
Steven Joseph <stevenchibuike99@gmail.com>
2026-04-29 15:48:21 +0100

    Update views.py

--- my_multimedia_site/gallery/views.py
+++ my_multimedia_site/gallery/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def  gallery_view(request):
-    return HttpResponse("Gallery Home Page")
+     return render(request, 'gallery/gallery.html')
