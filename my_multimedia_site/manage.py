04256488483980a24d8feff4ec8932e5b03f07b9
Steven Joseph <stevenchibuike99@gmail.com>
2026-05-04 14:58:40 +0100

    Update manage.py

--- my_multimedia_site/manage.py
+++ my_multimedia_site/manage.py
@@ -15,6 +15,10 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multimedia_site.settings')
     try:
         from django.core.management import execute_from_command_line
+        import django
+        django.setup()
+        create_admin()
+    
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
